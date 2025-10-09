5b2628eb65c5d3f5013fa05472b6f1f745d1ac4d
timglabisch <tim@ainfach.de>
2025-10-09 14:30:49 +0200

    ...

--- apps_xxx/main/src/index.tsx
+++ apps_xxx/main/src/index.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import {HelloFromLibA} from "libA/src";
 
 const rootEl = document.getElementById('root');
 if (rootEl) {
   const root = ReactDOM.createRoot(rootEl);
   root.render(
     <React.StrictMode>
+        <HelloFromLibA/>
       <App />
     </React.StrictMode>,
   );
