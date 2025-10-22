4294c9f63faa461fa55dd56be60f17184de4c26b
timglabisch <glabisch@easybill.de>
2025-10-22 16:54:31 +0200

    ...

--- apps_xxx/main/@mf-types/new_shadcn_app/NewShadcnExampleComponent.d.ts
+++
@@ -1,2 +0,0 @@
-export * from './compiled-types/src/NewShadcnExampleComponent';
-export { default } from './compiled-types/src/NewShadcnExampleComponent';
--- apps_xxx/main/@mf-types/new_shadcn_app/apis.d.ts
+++
@@ -1,3 +0,0 @@
-
-    export type RemoteKeys = 'new_shadcn_app/NewShadcnExampleComponent';
-    type PackageType<T> = T extends 'new_shadcn_app/NewShadcnExampleComponent' ? typeof import('new_shadcn_app/NewShadcnExampleComponent') :any;
